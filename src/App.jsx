3bebd3a1624ba8d605677cd1cc66eb143498bb02
K0W4 <kowaleskigabriel@gmail.com>
2024-10-10 15:29:07 -0300

    Modificações para suporte 3D

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import { Home, About, Projects, Contact } from "./pages";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -7,10 +9,10 @@ const App = () => {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={'Home'} />
-                <Route path="/about" element={'About'} />
-                <Route path="/projects" element={'Projects'} />
-                <Route path="/contact" element={'Contact'} />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
     </main>
