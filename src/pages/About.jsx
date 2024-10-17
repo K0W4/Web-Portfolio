11d045854077111bbaae77c6e390d0c1574c4c91
K0W4 <kowaleskigabriel@gmail.com>
2024-10-17 00:42:46 -0300

    Finalizando Contact

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+
+import { skills } from '../constants';
+import CTA from '../components/CTA';
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm <span className="blue-gradient_text font-semibold drop-shadow">Gabriel Kowaleski</span>!
+      </h1>
+
+      <div className="mt-5 flex flex-col ga3 text-slate-500">
+        <p>
+        A Software Engineer, born in Brazil, undergraduate student, seeking specialization in front-end technologies and as a game developer.</p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <hr className="border-slate-200" />
+
+      <CTA />
+    </section>
   )
 }
 
